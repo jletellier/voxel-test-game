40ce218dc1f1b9d5df02c480456078dd208c07dd
Julien Letellier <letellier.julien@gmail.com>
2014-04-21 11:47:54 +0200

    Added oculus rift support (using OculusBridge to get orientation updates)

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var THREE = require('three');
+var OculusBridge = require('./lib/OculusBridge.js');
+var oculus = require('./lib/voxel-oculus-vr');
 var createEngine = require('voxel-engine');
 
 var game = createEngine({
@@ -10,12 +13,12 @@ var game = createEngine({
     materialFlatColor: false,
     worldOrigin: [ 0, 0, 0 ],
     lightsDisabled: true,
-    controls: { discreteFire: true }
+    controls: { discreteFire: true, rotationZClamp: Math.PI / 4 }
 });
 game.appendTo(document.body);
 
 var createPlayer = require('voxel-player')(game);
-var avatar = window.avatar = createPlayer();
+var avatar = createPlayer();
 avatar.position.set(0, 20, 0);
 avatar.possess();
 
@@ -69,3 +72,40 @@ var sky = createSky();
 game.on('tick', function(delta) {
     sky(delta);
 });
+
+// try to connect to oculus
+var oculusEffect = null;
+var bridge = new OculusBridge({
+    'onConnect': function() {
+        //if (!oculusEffect) oculusEffect = new oculus(game);
+        console.log('we are connected!');
+    },
+    'onDisconnect': function() {
+        console.log('good bye Oculus.');
+    },
+    'onConfigUpdate': function(config) {
+        if (!oculusEffect) oculusEffect = new oculus(game, config);
+        console.log('Config: ', config);
+    },
+    'onOrientationUpdate': bridgeOrientationUpdated,
+    'onAccelerationUpdate': function(accelValues) {
+        var values = [accelValues.x, accelValues.y, accelValues.z];
+        //console.log("Acceleration: " + values.join(", "));
+    }
+});
+
+bridge.connect();
+
+var camEuler = new THREE.Euler();
+
+function bridgeOrientationUpdated(quatValues) {
+    camEuler.setFromQuaternion(quatValues, 'YZX');
+
+    target.pitch.rotation.x = clamp(camEuler.x, game.controls.x_rotation_clamp);
+    target.yaw.rotation.y = clamp(camEuler.y, game.controls.y_rotation_clamp);
+    target.roll.rotation.z = clamp(camEuler.z, game.controls.z_rotation_clamp);
+}
+
+function clamp(value, to) {
+    return isFinite(to) ? Math.max(Math.min(value, to), -to) : value
+}
